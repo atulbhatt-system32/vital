48296f2c337759f2328ec473f10c35d89868c145
atulbhatt-system32 <atulbhatt98@gmail.com>
2022-04-29 11:39:50 +0530

    removed imports

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <header className='App-header'></header>
+    </div>
+  );
 }
 
 export default App;

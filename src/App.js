5ec6f7ae442aba04bbbb1e82c9041d97f59833fa
atulbhatt-system32 <atulbhatt98@gmail.com>
2022-04-30 12:11:21 +0530

    Added Header and hero section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { logo } from './assets/img';
+
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'></header>
+      <header className='App-header'>
+        <nav>
+          <div className='nav_mobile'>
+            <img src={logo} alt='brand logo' />
+            <span className='brand_name'>VITAL</span>
+          </div>
+        </nav>
+      </header>
+      <main>
+        <section className='hero_section mobile'>
+          <div className='hero_section-content'>
+            <span>Your fully decentralised money pot</span>
+          </div>
+        </section>
+      </main>
     </div>
   );
 }

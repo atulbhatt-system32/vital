eb824fff8e13c4bd68b78dca8447cf801cbc0fb8
atulbhatt-system32 <atulbhatt98@gmail.com>
2022-04-30 17:25:13 +0530

    Desktop mode added for header

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { logo } from './assets/img';
+import { GradientBtn } from './components';
 import Home from './pages/Home/Home';
 
 function App() {
@@ -9,6 +10,23 @@ function App() {
           <div className='nav_mobile'>
             <img src={logo} alt='brand logo' />
             <span className='brand_name'>VITAL</span>
+          </div>
+          <div className='nav_desktop'>
+            <div className='brand_logo_wrapper'>
+              <img src={logo} alt='brand logo' />
+              <span className='brand_name'>VITAL</span>
+            </div>
+            <div className='nav_links'>
+              <div className='links_wrapper'>
+                <a href='#'>Home</a>
+                <a href='#'>About Us</a>
+                <a href='#'>Projects</a>
+                <a href='#'>Contact</a>
+              </div>
+              <div className='connect_wrapper'>
+                <GradientBtn text='Connect' />
+              </div>
+            </div>
           </div>
         </nav>
       </header>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -26,7 +26,24 @@ const Home = () => {
         </section>
 
         <section className={[styles.hero_section, styles.desktop].join(' ')}>
-          <div className={styles.hero_section_content}></div>
+          <div className={styles.hero_section_content}>
+            <div className={styles.hero_section_left}>
+              <div className={styles.hero_section_text_wrapper}>
+                <img src={hero_text} alt='your fully decentralized money pot' />
+              </div>
+              <div className={styles.card_wrapper}>
+                <BasicCard>
+                  <span className={styles.card_text}>
+                    Create, share, participate securely in a transparent and
+                    automated way
+                  </span>
+                  <div className={styles.gradient_btn_wrapper}>
+                    <GradientBtn text='Create' />
+                  </div>
+                </BasicCard>
+              </div>
+            </div>
+          </div>
         </section>
         {/* HERO SECTION END */}
       </main>

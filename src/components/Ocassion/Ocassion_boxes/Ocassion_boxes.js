ec67b4c0bd805d26b571a441b777faaeaa261091
atulbhatt-system32 <atulbhatt98@gmail.com>
2022-05-05 13:58:48 +0530

    styled: ocassion

--- src/components/Ocassion/Ocassion_boxes/Ocassion_boxes.js
+++ src/components/Ocassion/Ocassion_boxes/Ocassion_boxes.js
@@ -3,7 +3,7 @@ import GradientBtn from "../../Buttons/GradientButton/GradientBtn";
 import styles from "./Ocassion_boxes.module.scss";
 export default function Ocassion_boxes(props) {
   return (
-    <div className={styles.Ocassion_box}>
+    <div sel={props.title} className={styles.Ocassion_box}>
       <h3 className={styles.Ocassion_box_head}>{props.title}</h3>
       <p className={styles.Ocassion_box_description}>
         Create your kitty for all types of needs: Wedding, birthday, retirement,

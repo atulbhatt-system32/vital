bcedd720f894e6087aa6d6224097ce0255b4f704
atulbhatt-system32 <atulbhatt98@gmail.com>
2022-05-06 15:49:12 +0530

    Style: Desktop Screen

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -7,46 +7,48 @@ export default function Projects() {
       <div className={styles.projects_wrap}>
         {/* HEAD*/}
         <h1 className={styles.projects_head}>search by categories</h1>
-        {/* INPUT BOX */}
-        <div className={styles.projects_inputBox}>
-          <div className={styles.projects_inputBox_wrap}>
-            {/* NAME */}
-            <div className={styles.projects_inputBox_name}>
-              <div className={styles.projects_inputBox_name_text}>Name</div>
-              <input
-                placeholder="Enter your name"
-                className={styles.projects_inputBox_name_input}
-              ></input>
-            </div>
-            {/* TYPE */}
-            <div className={styles.projects_inputBox_type}>
-              <div className={styles.projects_inputBox_type_text}>Type</div>
-              <input
-                placeholder="Select type"
-                className={styles.projects_inputBox_type_input}
-              ></input>
-            </div>
-            {/* CATEGORY */}
-            <div className={styles.projects_inputBox_category}>
-              <div className={styles.projects_inputBox_category_text}>
-                Category
+        <div className={styles.projects_wrap_container}>
+          {/* INPUT BOX */}
+          <div className={styles.projects_inputBox}>
+            <div className={styles.projects_inputBox_wrap}>
+              {/* NAME */}
+              <div className={styles.projects_inputBox_name}>
+                <div className={styles.projects_inputBox_name_text}>Name</div>
+                <input
+                  placeholder="Enter your name"
+                  className={styles.projects_inputBox_name_input}
+                ></input>
               </div>
-              <input
-                placeholder="Select type"
-                className={styles.projects_inputBox_category_input}
-              ></input>
-            </div>
-            {/* BUTTON */}
-            <div className={styles.projects_btn_wrap}>
-              <GradientBtn text="Search" />
+              {/* TYPE */}
+              <div className={styles.projects_inputBox_type}>
+                <div className={styles.projects_inputBox_type_text}>Type</div>
+                <input
+                  placeholder="Select type"
+                  className={styles.projects_inputBox_type_input}
+                ></input>
+              </div>
+              {/* CATEGORY */}
+              <div className={styles.projects_inputBox_category}>
+                <div className={styles.projects_inputBox_category_text}>
+                  Category
+                </div>
+                <input
+                  placeholder="Select type"
+                  className={styles.projects_inputBox_category_input}
+                ></input>
+              </div>
+              {/* BUTTON */}
+              <div className={styles.projects_btn_wrap}>
+                <GradientBtn text="Search" />
+              </div>
             </div>
           </div>
+          <div className={styles.projects_wrap_container_projects}>
+            {[1, 2, 3, 4].map((item) => (
+              <ProjectCard key={item} />
+            ))}
+          </div>
         </div>
-      </div>
-      <div className={styles.projectcards_wrapper}>
-        {[1, 2, 3, 4].map((item) => (
-          <ProjectCard key={item} />
-        ))}
       </div>
     </div>
   );
